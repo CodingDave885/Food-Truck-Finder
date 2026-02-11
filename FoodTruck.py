bd9db46506951cfa821a6f354d1824813690e4dc
dal366 <dal366@drexel.edu>
2026-02-10 23:32:56 -0500

    Oops, forgot to add comment on what helper route was

--- FoodTruck.py
+++ FoodTruck.py
@@ -33,6 +33,8 @@ class MenuItem(db.Model):
 @app.route("/")
 def home():
     return render_template("home.html", title="Home")
+
+# This route leads you to Alex's page that he made saying food truck information
 @app.route("/helper")
 def info():
     return render_template("TruckItemsRaw.html")
