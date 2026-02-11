8095306a59960489f67e6f3f2abed536c2688912
dal366 <dal366@drexel.edu>
2026-02-10 23:18:13 -0500

    Added new page and declared database for truck

--- FoodTruck.py
+++ FoodTruck.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class Truck(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 
 @app.route("/")
 def home():
     return render_template("home.html", title="Home")
-
+@app.route("/helper")
+def info():
+    return render_template("TruckItemsRaw.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
