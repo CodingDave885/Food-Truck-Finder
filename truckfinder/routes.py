1a4459201107b8531f89b5ff2cd205169ea2e7fc
an3243 <an3243@drexel.edu>
2026-02-16 18:56:38 -0500

    Changed website aesthetics (colors, fonts, added seperate map page) Overhauled whole website.

--- truckfinder/routes.py
+++ truckfinder/routes.py
@@ -6,6 +6,14 @@ from flask import render_template, url_for
 def home():
     return render_template("home.html", title="Home")
 
+@app.route('/map')
+def map_page():
+    return render_template('Map.html')
+
+@app.route('/about')
+def about():
+    return render_template('About.html')
+
 # This route leads you to Alex's page that he made saying food truck information
 @app.route("/helper")
 def info():
