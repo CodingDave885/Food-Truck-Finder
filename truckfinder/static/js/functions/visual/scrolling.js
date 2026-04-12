bf53dbeb624c638950eb98824f31ecb9ce6ea0c1
an3243 <an3243@drexel.edu>
2026-04-12 17:09:22 -0400

    Added feature cards, and a small statement and statistics section in our home page.

---
+++ truckfinder/static/js/functions/visual/scrolling.js
@@ -0,0 +1,16 @@
+// Authored : Andre Nunes da Silva @ 4:24 PM
+
+const observer = new IntersectionObserver((entries) => { // Creates an observer that watches for elements entering/leaving the viewport
+    entries.forEach(entry => { // loops thru every element being watched
+      if (entry.isIntersecting) { // if its visible on the screen as a class called visible
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible'); // if not visible anymore remove
+      }
+    });
+  }, { threshold: 0.15 }); // 15% of the element being in view
+
+  // Find every card and section blob on the page and watch them with the observer
+  document.querySelectorAll('.card, .middle, .section-blob, .stat-card, .about-left').forEach(el => {
+    observer.observe(el);
+  });
